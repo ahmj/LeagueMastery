feabccd1afa3a4129ab6d5c72b2122d3c7b6d1d8
ahmj <abdullah.miraj@mail.utoronto.ca>
2016-04-28 02:24:37 -0400

    Added caching, retrieving and processing champion mastery data

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,17 @@ require('dotenv').config();
 var express = require('express');
 var routes = require('./routes/index');
 var masteries = require('./routes/masteries');
+
 var port = process.env.PORT || 3000;
 var app = express();
 
 app.use('/', routes);
-app.use('/mastery', mastery);
+app.use('/mastery', masteries);
 
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.send(err.message);
+});
 app.listen(3000, function() {
 	console.log('App listening on port: 3000');
 });
--- backend/routes/masteries.js
+++ backend/routes/masteries.js
@@ -1,6 +1,13 @@
 var express = require('express');
-var router = express.Router();
+var api = require('leagueapi');
+var nodeCache = require('node-cache');
 
+var router = express.Router();
+var cache = new nodeCache({stdTTL: 3600, checkperiod: 300});
+
+var CHAMPION_SCORE_FACTOR = 5;
+
+api.init(process.env.API_KEY);
 router.use(function summonerLog(req, res, next) {
 	if (req.params.id) {
 		console.log("Summoner: " + req.params.id + "Time: " + Date.now());
@@ -11,17 +18,61 @@ router.get('/', function(req, res, next) {
 	res.send("Hello!");
 });
 
-router.get('/:summoner', function(req, res, next) {
+router.get('/:summoner/:region', function(req, res, next) {
+	var summoner = (req.params.summoner).toLowerCase();
+	var region = req.params.region;
 
+	var cacheKey = summoner + region;
+	var cached = cache.get(cacheKey);
+	if (cached) {return res.json(cached);}
+	
+	api.Summoner.getByName(summoner, region, function(err, summonerName) {
+		if (err) return next(err); 
+		var id = summonerName[summoner].id;
+		api.ChampionMastery.getChampions(id, region, function(err, champions) {
+			if (err) return next(err);
+			var data = [];
+			for (var i=0; i < champions.length; i++) {
+				if (!champions[i].chestGranted) {
+					data.push(calculateScore(champions[i]));
+				}
+			}
+			data.sort(function (a, b) {return b.score - a.score});
+			cache.set(cacheKey, data);
+			return res.json(data);
+		});
+	});
 });
 
+function calculateScore(champion) {
+	var champion_grade_factor = 0;
+	if (champion.highestGrade) {
+		champion_grade_factor = calculateGradeScore(champion.highestGrade);
+	}
+	champion.score = champion_grade_factor + (CHAMPION_SCORE_FACTOR * champion.championLevel);
+	return champion;
+}
+
+function calculateGradeScore(grade) {
+	var score = 0;
+	if (grade[0] === "S") {
+		score = 13;
+	}
+	if (grade[0] === "A") {
+		score = 7; 
+	}
+	if (grade[0] === "B") {
+		score = 3; 
+	}
+	if (grade[1] && grade[1] === "-") {
+		score -= 2;
+	} else if (grade[1] && grade[1] === "+") {
+		score += 3;
+	}
+	return score;
+}
 module.exports = router;
 /*
-TODO: Convert summoner name to id
-	  If cache -> return
-	  else
-	  Grab Mastery Data
-	  Process results
-	  Store in redis cache
-	  Send response
+TODO: - Incorperate champion.gg roles and winrates
+	  - Add names of champions to data set
 */
