610ad1fbfd5a3c1a456efdfb171606ecd5655618
ahmj <abdullah.miraj@mail.utoronto.ca>
2016-04-30 18:40:39 -0400

    Missing changes in tasks.js for last commit

--- backend/tasks/tasks.js
+++ backend/tasks/tasks.js
@@ -2,10 +2,12 @@ var championStaticData = require('./champion-static-data');
 var ggStaticData = require('./gg-static-data');
 module.exports = {
 	init: function(callback) {
-		championStaticData.run(function() {
+		championStaticData.run(function(err) {
+			if (err) {return console.log("Error when retrieving champion static data...");}
 			console.log("Champion Static data updated: " + new Date().toISOString());
 		});
-		ggStaticData.run(function() {
+		ggStaticData.run(function(err) {
+			if (err) {return console.log("Error when retrieving GG static data...");}
 			console.log("GG Static data updated: " + new Date().toISOString());
 		});
 	}
