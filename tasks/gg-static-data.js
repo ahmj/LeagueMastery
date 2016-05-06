5f878af860d7f047b2154a77b0e7d8ca404c8eb1
ahmj <abdullah.miraj@mail.utoronto.ca>
2016-05-05 21:34:18 -0400

    Another fix for champion gg static api json parsing

--- tasks/gg-static-data.js
+++ tasks/gg-static-data.js
@@ -15,8 +15,8 @@ module.exports = {
 			api.statistics.all(function(err, data) {
 				if (err) {return callback(err);}
 				var json;
-				try {json = JSON.parse(data);}
-				catch(e) {json = JSON.stringify(data)}
+				var objectConstructor = {}.constructor;
+				json = (data.constructor === objectConstructor) ? data : JSON.stringify(data);
 				fs.writeFile(fileName, json, 'utf8', callback);
 				return;
 			});
