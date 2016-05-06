47295b12d60bc081136a0b8d8bc08c344fac0227
ahmj <abdullah.miraj@mail.utoronto.ca>
2016-05-05 21:38:29 -0400

    Added error messages when failed tasks occur

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -5,19 +5,19 @@ var ggChampData = require('./gg-champ-data');
 module.exports = {
 	init: function(callback) {
 		championStaticData.run(function(err) {
-			if (err) {return console.log("Error when retrieving champion static data...");}
+			if (err) {console.log("Error when retrieving champion static data..."); return console.log(err);}
 			console.log("Champion Static data updated: " + new Date().toISOString());
 			ggChampData.run(function(err) {
-				if (err) {return console.log("Error when retrieving championgg data...");}
+				if (err) {console.log("Error when retrieving championgg data..."); return console.log(err);}
 				console.log("ChampionGG data updated: " + new Date().toISOString());
 			})
 		});
 		ggStaticData.run(function(err) {
-			if (err) {return console.log("Error when retrieving GG static data...");}
+			if (err) {console.log("Error when retrieving GG static data..."); return console.log(err);}
 			console.log("GG Static data updated: " + new Date().toISOString());
 		});
 		ggChampData.run(function(err) {
-			if (err) {return console.log("Error when retrieving championgg data...");}
+			if (err) {console.log("Error when retrieving championgg data..."); return console.log(err);}
 			console.log("ChampionGG data updated: " + new Date().toISOString());
 		})
 	}
